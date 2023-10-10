65973317ac077e65fa9588b3d92dcf07508bcf60
Hoang Nguyen <hdnguyen0305@gmail.com>
2023-10-10 14:37:00 +0700

    update after presentation

--- content/posts/practical-react-query-3/comparison.tsx
+++ content/posts/practical-react-query-3/comparison.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Table } from 'antd'
 import './style.css'
 
-export function FetchingComparison() {
+export function ClientStateManagementComparison() {
   const data = [
     {
       usestate: 'Dễ sử dụng và hiểu',
--- src/components/rq-toc.tsx
+++ src/components/rq-toc.tsx
@@ -6,77 +6,23 @@ type Props = {
 }
 
 const mapping = [
-  { id: 'practical-react-query', title: '#1: Practical React Query' },
   {
-    id: 'react-query-data-transformations',
-    title: '#2: React Query Data Transformations',
+    id: 'client-state-vs-server-state',
+    title: '#1: Client State vs. Server State',
   },
   {
-    id: 'react-query-render-optimizations',
-    title: '#3: React Query Render Optimizations',
+    id: 'practical-react-query-1-tips-and-tricks',
+    title: '#2: Practical React Query 1 - tips and tricks',
   },
   {
-    id: 'status-checks-in-react-query',
-    title: '#4: Status Checks in React Query',
-  },
-  { id: 'testing-react-query', title: '#5: Testing React Query' },
-  {
-    id: 'react-query-and-type-script',
-    title: '#6: React Query and TypeScript',
+    id: 'practical-react-query-2-custom-hooks-and-query-key-factory',
+    title:
+      '#3: Practical React Query 2 - custom hooks and query key factory',
   },
   {
-    id: 'using-web-sockets-with-react-query',
-    title: '#7: Using WebSockets with React Query',
+    id: 'practical-react-query-3-client-state-zustand',
+    title: '#4: Practical React Query 3 - Client State Zustand',
   },
-  {
-    id: 'effective-react-query-keys',
-    title: '#8: Effective React Query Keys',
-  },
-  {
-    id: 'leveraging-the-query-function-context',
-    title: '#8a: Leveraging the Query Function Context',
-  },
-  {
-    id: 'placeholder-and-initial-data-in-react-query',
-    title: '#9: Placeholder and Initial Data in React Query',
-  },
-  {
-    id: 'react-query-as-a-state-manager',
-    title: '#10: React Query as a State Manager',
-  },
-  {
-    id: 'react-query-error-handling',
-    title: '#11: React Query Error Handling',
-  },
-  {
-    id: 'mastering-mutations-in-react-query',
-    title: '#12: Mastering Mutations in React Query',
-  },
-  { id: 'offline-react-query', title: '#13: Offline React Query' },
-  { id: 'react-query-and-forms', title: '#14: React Query and Forms' },
-  { id: 'react-query-fa-qs', title: '#15: React Query FAQs' },
-  {
-    id: 'react-query-meets-react-router',
-    title: '#16: React Query meets React Router',
-  },
-  {
-    id: 'seeding-the-query-cache',
-    title: '#17: Seeding the Query Cache',
-  },
-  { id: 'inside-react-query', title: '#18: Inside React Query' },
-  { id: 'type-safe-react-query', title: '#19: Type-safe React Query' },
-  {
-    id: 'you-might-not-need-react-query',
-    title: '#20: You Might Not Need React Query'
-  },
-  {
-    id: 'thinking-in-react-query',
-    title: '#21: Thinking in React Query'
-  },
-  {
-    id: 'react-query-and-react-context',
-    title: '#22: React Query and React Context'
-  }
 ]
 
 export const RqToc = ({ id }: Props) => {
@@ -88,7 +34,7 @@ export const RqToc = ({ id }: Props) => {
         'list-style-type': 'none',
         gap: 0.7,
         paddingTop: 2,
-        paddingBottom: 4
+        paddingBottom: 4,
       }}
     >
       {mapping.map((item) => {
